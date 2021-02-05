d58c0e4f85ccd76d75f3bc77eadf14c48fa2da2e
Chiara Mancuso <chia.mancuso@gmail.com>
2021-02-05 11:32:35 +0100

    Resolution

--- calibration.py
+++ calibration.py
@@ -12,8 +12,15 @@ def Cal(E,a):
     return(i)
 
 def Reso(E,k):
-    i=2.35*np.sqrt(k/E)
+    e = np.sqrt(1./E)
+    i=2.35*k*e
     return(i)
+
+def chi2(E, R, err, *p):
+    chi2=sum(((R - Reso(E,*p) )/ err) ** 2)
+    dof=len(E)-2
+    #print(chi2, dof)
+    return round(chi2/dof,2)
 
 def Calibration(ADC,err_ADC,E_exp,Channel_number):  ##Channel number is a word; ex: "channel 2"
     """Calibration fit C=f(E)"""
@@ -38,7 +45,7 @@ def Calibration(ADC,err_ADC,E_exp,Channel_number):  ##Channel number is a word; 
 
     return ([a,err_a])
 
-def Resolution(ADC,err_ADC,ADC_Width,err_ADC_width,a,err_a,Channel_number):    ##a is the coeff ADC=a*E so in kev-1 and Channel number is a word; ex: "channel 2"
+def Resolution(ADC,err_ADC,ADC_width,err_ADC_width,a,err_a,Channel_number):    ##a is the coeff ADC=a*E so in kev-1 and Channel number is a word; ex: "channel 2"
 
     """Resolution fit"""
     ###First define E and E_width with their respective uncertainties
@@ -46,17 +53,20 @@ def Resolution(ADC,err_ADC,ADC_Width,err_ADC_width,a,err_a,Channel_number):    #
     err_E=np.sqrt((err_ADC/a)**2+(ADC*err_a/a**2)**2)
 
     E_Width=ADC_width/a
-    err_E_Width=np.sqrt((err_ADC_Width/a)**2+(ADC_Width*err_a/a**2)**2)
+    err_E_Width=np.sqrt((err_ADC_width/a)**2+(ADC_width*err_a/a**2)**2)
 
     ###Define the resolution as R
     R=E_Width/E
     err_R=np.sqrt((err_E_Width/E)**2+(err_E*E_Width/E**2)**2)
 
-    par_Res, par_Res_var=curve_fit(Res,E,R,sigma=err_R,absolute_sigma=True)
+    par_Res, par_Res_var=curve_fit(Reso,E,R,sigma=err_R,absolute_sigma=True)
+
+    k=round(par_Res[0]**2,3)
+    err_k=round(np.sqrt(np.diag(par_Res_var)[0])*par_Res[0]*np.sqrt(2),3)
 
     #plt.figure(Channel_number)
     plt.errorbar(E,R,xerr=err_E,yerr=err_R,fmt='b.',capsize=4)
-    plt.plot(E,Res(np.linspace(min(E),max(E),1000),par_Res[0]),c='k',label="ajustement function")
+    plt.plot(np.linspace(min(E),max(E),1000),Reso(np.linspace(min(E),max(E),1000),par_Res[0]),c='k',label="k=%s"%k+"$\pm$%s"%err_k)
     plt.title('Resolution R=f(E)',fontsize=20)
     plt.xlabel('E (keV)')
     plt.ylabel('R')
@@ -64,10 +74,8 @@ def Resolution(ADC,err_ADC,ADC_Width,err_ADC_width,a,err_a,Channel_number):    #
     plt.show()
     plt.savefig('Resolution_channel_{}'.format(Channel_number))
 
-    k=par_Res[0]**2
-    err_k=np.sqrt(np.diag(par_Res_var)[0])*par_Res[0]*np.sqrt(2)
-
-    return(Channel_number," k=", k,"+-",err_k,"keV")
+    chi = chi2(E, R, err_R, *par_Res)
+    return(Channel_number, k, err_k, chi)
 
 
 
@@ -97,9 +105,17 @@ Ch3=(np.array(Ch3)).astype(float)
 
 
 E_exp=np.array([1173.,1332.,511.,1275.,662.]) ##expected energy of the pics from the spectrums Co Na Cs in keV
+a=[]
+a.append(Calibration(Ch1[:,2],Ch1[:,3],E_exp,1))
+a.append(Calibration(Ch2[:,2],Ch2[:,3],E_exp,2))
+a.append(Calibration(Ch3[:,2],Ch3[:,3],E_exp,3))
+print(a[1][1])
 
-print(Calibration(Ch1[:,2],Ch1[:,3],E_exp,1))
-print(Calibration(Ch2[:,2],Ch2[:,3],E_exp,2))
-print(Calibration(Ch3[:,2],Ch3[:,3],E_exp,3))
+g = [["Channel", "k", "Error", "chi2"]]
+g.append(Resolution(Ch1[:,2],Ch1[:,3],Ch1[:,4],Ch1[:,5],a[0][0],a[0][1],1))
+g.append(Resolution(Ch2[:,2],Ch2[:,3],Ch2[:,4],Ch2[:,5],a[1][0],a[1][1],2))
+g.append(Resolution(Ch3[:,2],Ch3[:,3],Ch3[:,4],Ch3[:,5],a[2][0],a[2][1],3))
 
-# Resolution(Ch1[:,2],Ch1[:,3],Ch1[:,4],Ch1[:,5],a,err_a,Channel_number)
+np.savetxt("Resolution.csv", g, delimiter="\t", fmt='%s')
+
+#Resolution(ADC,err_ADC,ADC_Width,err_ADC_width,a,err_a,Channel_number)
