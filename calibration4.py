f0409ba07cc2aeeae123b24586accdba715fa701
Chiara Mancuso <chia.mancuso@gmail.com>
2021-02-05 14:29:00 +0100

    new new

--- calibration4.py
+++ calibration4.py
@@ -3,7 +3,7 @@ from scipy.stats import norm
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.optimize import curve_fit
-from terminaltables import AsciiTable
+#from terminaltables import AsciiTable
 import csv
 import pandas
 plt.rcParams['axes.facecolor'] = 'white'
